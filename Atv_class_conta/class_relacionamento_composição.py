6d8c284b98f74c5f541f1487adb894508921d27d
projectsifma <lucielcastro@acad.ifma.edu.br>
2022-11-09 11:20:07 -0300

    AJUSTES

--- Atv_class_conta/class_relacionamento_composição.py
+++ Atv_class_conta/class_relacionamento_composição.py
@@ -1,134 +1,138 @@
 
 class Cliente:
-   def __init__(self,nome, cpf, telefone):
+   def __init__(self,nome:str, cpf, telefone):
       self.__nome = nome
       self.__cpf = cpf
       self.__telefone = telefone
-      self.__contas = []
-      self.__chaves = []
+      self.contas = None
 
    @property
-   def get_nome(self):
+   def nome_(self):
       return self.__nome
       
-   @get_nome.setter
-   def set_nome(self, nome):
+   @nome_.setter
+   def nome_(self, nome):
       self.__nome = nome
    
    @property
-   def get_cpf(self):
+   def cpf(self):
       return self.__cpf
    
-   @get_cpf.setter
-   def set_cpf(self, cpf):
+   @cpf.setter
+   def cpf(self, cpf):
       self.__cpf = cpf
    
    @property
-   def get_telefone(self):
+   def telefone(self):
       return self.__telefone
    
-   @get_telefone.setter
-   def set_telefone(self, telefone):
+   @telefone.setter
+   def telefone(self, telefone):
       self.__telefone = telefone
    
    #composição Conta
-   def insereConta(self,numConta, agencia, saldo):
-      self.__contas.append(Conta(numConta, agencia, saldo))
+   def criar_conta(self,numConta, agencia, saldo):
+      self.contas=(Conta(numConta, agencia, saldo))
    
-   def listaContas(self):
-      for conta in self.__contas:
-         print(f'Agencia: {conta.agencia}, Conta: {conta.numConta}, Saldo: {conta.saldo}')
+   def imprimir_dados_pessoas(self):
+      print(f'\nNome: {self.__nome} CPF: {self.__cpf}, Telefone: {self.__telefone}')
    
-   def ver_saldo(self):
-      for conta in self.__contas:
-         print(f'{self.__nome}, seu saldo é de R${conta.saldo}')
+   def imprimir_dados_conta(self):
+         print(f'Conta: {self.contas.numConta}, Agencia: {self.contas.agencia}')
 
-   def depositar(self, valor):
-      for conta in self.__contas:
-         conta.saldo += valor
-
-   def transferir(self, valor, contaDestino):
-      for conta in self.__contas:
-         conta.saldo -= valor
-         contaDestino.depositar(valor)
    
-   #Composição Chave
-   def insereChave(self, chave_pix):
-      self.__chaves.append(ChavePix(chave_pix))
-   
-   def listaChaves(self):
-      for chave in self.__chaves:
-         print(f'Chave Pix{chave.chave_pix}')
-
-   def __del__(self):
-      print(f'{self.__nome} foi apagado')
-
-         
 class Conta:
    def __init__(self,numConta, agencia, saldo):
       self.__numConta = numConta
       self.__agencia = agencia
       self.__saldo = saldo
+      self.chaves = []
       
    @property
-   def get_numConta(self):
+   def numConta(self):
       return self.__numConta
    
-   @get_numConta.setter
-   def set_numconta(self, numconta):
+   @numConta.setter
+   def numconta(self, numconta):
       self.__numConta = numconta
    
    @property
-   def get_agencia(self):
+   def agencia(self):
       return self.__agencia
    
-   @get_agencia.setter
-   def set_agencia(self, agencia):
+   @agencia.setter
+   def agencia(self, agencia):
       self.__agencia = agencia
    
    @property
-   def get_saldo(self):
+   def saldo(self):
       return self.__saldo
    
-   @get_saldo.setter
-   def set_saldo(self, saldo):
+   @saldo.setter
+   def saldo(self, saldo):
       self.__saldo = saldo
    
+   def criar_chave_pix(self, chave_pix):
+      self.chaves.append(ChavePix(chave_pix))
+   
+   def listaChaves(self):
+      for chave in self.chaves:
+         print(f'Chave Pix: {chave.chave_pix}')
+   
+   def ver_saldo(self):
+      #for conta in self.contas:
+         print(f'Seu saldo é de R$ {self.__saldo}')
+   
+
+   def depositar(self, valor):
+         self.__saldo += valor
+         print('Deposito realizado com sucesso!')
+
+   def transferir(self, valor, contaDestino):
+         self.__saldo -= valor
+         contaDestino.depositar(valor)
+         
+         print('Transferência realizado com sucesso!')
 
 class ChavePix:
    def __init__(self, chave_pix):
       self.__chave_pix = chave_pix
    
    @property
-   def get_chave_pix(self,):
+   def chave_pix(self):
       return self.__chave_pix
 
-   @get_chave_pix.setter
+   @chave_pix.setter
    def set_chave_pix(self, chave_pix):
       self.__chave_pix = chave_pix
 
-c1 = Cliente('Daniela','38784-44','9389-33')
-c1.insereConta('1345-38','3232',344)
-c1.insereChave('lucas@gmail.com')
-print(c1.nome)
-c1.listaContas()
-c1.listaChaves()
-c1.ver_saldo()
-c1.depositar(10)
-c1.ver_saldo()
 
-c2 = Cliente('Junior','9398-323','3232-33')
-c2.insereConta('233-3','434',200)
-c2.insereChave('junior@gmail.com')
-print(c2.nome)
-c2.listaContas()
-c2.listaChaves()
-c2.ver_saldo()
-c2.depositar(10)
-c2.ver_saldo()
+cliente1 = Cliente('Daniela','38784-44','9389-33')
+cliente1.criar_conta('1345-38','3232',344)
+cliente1.contas.criar_chave_pix('lucas@gmail.com')
 
-valor = float(input('Valor a transferir: '))
-c1.transferir(valor, c2)
-c1.ver_saldo()
-c2.ver_saldo()
+cliente1.imprimir_dados_pessoas()
+cliente1.imprimir_dados_conta()
+cliente1.contas.listaChaves()
+cliente1.contas.ver_saldo()
+cliente1.contas.depositar(10)
+cliente1.contas.ver_saldo()
+
+
+cliente2 = Cliente('Luciel','12784-44','0089-33')
+cliente2.criar_conta('4434-38','656',100)
+cliente2.contas.criar_chave_pix('luciel@gmail.com')
+
+cliente2.imprimir_dados_pessoas()
+cliente2.imprimir_dados_conta()
+cliente2.contas.listaChaves()
+cliente2.contas.ver_saldo()
+cliente2.contas.depositar(60)
+cliente2.contas.ver_saldo()
+
+
+#valor = float(input('\nValor a transferir: '))
+cliente1.contas.transferir(40, cliente2.contas)
+cliente1.contas.ver_saldo()
+cliente2.contas.ver_saldo()
 
